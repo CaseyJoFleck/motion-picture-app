d56056f9f9c2f3453c18b292c41da33b2a158653
Casey Fleck <casejofleck@gmail.com>
2022-11-19 17:00:38 -0800

    style(header): re-style website header and background color

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import { Component, CSSProperties, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import cx from "classnames";
 import { ReactSearchAutocomplete } from "react-search-autocomplete";
+import Header from "./Header.js";
 
 const Home = () => {
   const [existingMovies, setExistingMovies] = useState([
@@ -56,18 +57,20 @@ const Home = () => {
   const handleOnSearch = async (string, results) => {
     try {
       const res = await fetch(
-        `https://api.themoviedb.org/3/search/multi?api_key=***REMOVED***&language=en-US&page=1&query=` +
+        `https://api.themoviedb.org/3/search/multi?api_key=***REMOVED***&language=en-US&page=1&include_adult=false&query=` +
           string
       );
       const data = await res.json();
-      const updatedOptions = data.results.map((p) => {
-        return {
-          id: uuidv4(),
-          title: p.name === undefined ? p.title : p.name,
-          tmd_id: p.id,
-          description: p.overview,
-        };
-      });
+      const updatedOptions = data.results
+        .filter(({ media_type }) => media_type !== "person")
+        .map((p) => {
+          return {
+            id: uuidv4(),
+            title: p.name === undefined ? p.title : p.name,
+            tmd_id: p.id,
+            description: p.overview,
+          };
+        });
       setSearchResults(updatedOptions);
     } catch (err) {}
   };
@@ -83,7 +86,6 @@ const Home = () => {
       },
       ...existingMovies,
     ]);
-    setMovie(" ");
   };
 
   const formatResult = (item) => {
@@ -95,57 +97,60 @@ const Home = () => {
   };
 
   return (
-    <div className={styles.content}>
-      <h1 className={styles.h1}>Fleck Family Movies</h1>
-      <div>
-        <div>
-          <ReactSearchAutocomplete
-            items={searchResults}
-            //  value={movie}
-            //onChange={(e) => setMovie(e.target.value)}
-            fuseOptions={{ keys: ["title"] }} // Search on both fields
-            resultStringKeyName="title" // String to display in the results
-            onSearch={handleOnSearch}
-            onSelect={handleOnSelect}
-            inputSearchString={movie}
-            styling={{ zIndex: 1, fontSize: "calc(16px + 0.4vw)" }}
-            formatResult={formatResult}
-            placeholder={"Movies, Shows, Actors..."}
-            autoFocus
-          />
+    <>
+      <Header />
+      <div className={styles.content}>
+        {/* <h1 className={styles.h1}>Fleck Family Movies</h1> */}
+        <div className={styles.search}>
+          <div>
+            <ReactSearchAutocomplete
+              items={searchResults}
+              //  value={movie}
+              //onChange={(e) => setMovie(e.target.value)}
+              fuseOptions={{ keys: ["title"] }} // Search on both fields
+              resultStringKeyName="title" // String to display in the results
+              onSearch={handleOnSearch}
+              onSelect={handleOnSelect}
+              inputSearchString={movie}
+              styling={{ zIndex: 1 }}
+              formatResult={formatResult}
+              placeholder={"Movies, Shows, Actors..."}
+              autoFocus
+            />
+          </div>
+
+          <br />
+
+          <ul>
+            {existingMovies
+              .filter(({ watched }) => !watched)
+              .map(({ id, movie_name, watched }) => (
+                <li
+                  key={id}
+                  onClick={() => handleToggle(id)}
+                  className={cx(styles.existingMovie)}
+                >
+                  {movie_name}
+                </li>
+              ))}
+          </ul>
+
+          <ul>
+            {existingMovies
+              .filter(({ watched }) => watched)
+              .map(({ id, movie_name, watched }) => (
+                <li
+                  key={id}
+                  onClick={() => handleToggle(id)}
+                  className={cx(styles.existingMovie, styles.watched)}
+                >
+                  {movie_name}
+                </li>
+              ))}
+          </ul>
         </div>
-
-        <br />
-
-        <ul>
-          {existingMovies
-            .filter(({ watched }) => !watched)
-            .map(({ id, movie_name, watched }) => (
-              <li
-                key={id}
-                onClick={() => handleToggle(id)}
-                className={cx(styles.existingMovie)}
-              >
-                {movie_name}
-              </li>
-            ))}
-        </ul>
-
-        <ul>
-          {existingMovies
-            .filter(({ watched }) => watched)
-            .map(({ id, movie_name, watched }) => (
-              <li
-                key={id}
-                onClick={() => handleToggle(id)}
-                className={cx(styles.existingMovie, styles.watched)}
-              >
-                {movie_name}
-              </li>
-            ))}
-        </ul>
       </div>
-    </div>
+    </>
   );
 };
 
