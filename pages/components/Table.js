fed6ee839d1c72c5bb04b310eaddc20ec577782e
Casey Fleck <casejofleck@gmail.com>
2022-11-21 21:30:58 -0800

    feat(table): create new table for movie values

--- pages/components/Table.js
+++ pages/components/Table.js
@@ -1,36 +1,44 @@
 import cx from "classnames";
 import styles from "../../styles/styles.module.scss";
+import * as React from "react";
+import { DataGrid, GridRowsProp, GridColDef } from "@mui/x-data-grid";
+import GridCellExpand from "./GridCellExpand.js";
+
+function renderCellExpand(params) {
+  return (
+    <GridCellExpand
+      value={params.value || ""}
+      width={params.colDef.computedWidth}
+    />
+  );
+}
 
 const Table = ({ existingMovies, handleToggle }) => {
-  return (
-    <div>
-      <ul>
-        {existingMovies
-          .filter(({ watched }) => !watched)
-          .map(({ id, movie_name, watched }) => (
-            <li
-              key={id}
-              onClick={() => handleToggle(id)}
-              className={cx(styles.existingMovie)}
-            >
-              {movie_name}
-            </li>
-          ))}
-      </ul>
+  const rows = existingMovies.map(
+    ({ id, description, movie_name, watched }) => {
+      return {
+        id: id,
+        col1: movie_name,
+        col2: description,
+        col3: watched ? "Yes" : "No",
+      };
+    }
+  );
 
-      <ul>
-        {existingMovies
-          .filter(({ watched }) => watched)
-          .map(({ id, movie_name, watched }) => (
-            <li
-              key={id}
-              onClick={() => handleToggle(id)}
-              className={cx(styles.existingMovie, styles.watched)}
-            >
-              {movie_name}
-            </li>
-          ))}
-      </ul>
+  const columns = [
+    { field: "col1", headerName: "Title", width: 200 },
+    {
+      field: "col2",
+      headerName: "Description",
+      minWidth: 200,
+      renderCell: renderCellExpand,
+    },
+    { field: "col3", headerName: "Watched?", width: 150 },
+  ];
+
+  return (
+    <div style={{ height: 300, width: "100%" }}>
+      <DataGrid rows={rows} columns={columns} />
     </div>
   );
 };
